0cce947065c37812723a54b76639cd54d1b86f2f
Josh Espinoza <joshespi@gmail.com>
2021-12-04 08:23:48 -0700

    commented out currently non used assets. changed style sheet version number

--- template/header.php
+++ template/header.php
@@ -25,10 +25,10 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Genos&family=Indie+Flower&family=Press+Start+2P&family=Source+Code+Pro&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!--  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
   <!-- End Fonts -->
 
-  <link rel="stylesheet" href="assets/css/styles.css?v=1.0.0">
+  <link rel="stylesheet" href="assets/css/styles.css?v=0.0.1">
 </head>
 
 <body>
