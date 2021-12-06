233f9ffd2912ad7963e8ead4305ad583be3b8b82
Josh Espinoza <joshespi@gmail.com>
2021-12-05 23:47:20 -0700

    added jquery

--- template/footer.php
+++ template/footer.php
@@ -5,8 +5,7 @@
 <footer id="mainFooter">
 
 </footer>
-</main>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="assets/js/scripts.js?v=1.0.0"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="assets/js/scripts.js?v=0.0.1"></script>
 </body>
 </html>
