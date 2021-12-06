32814ee0fb046af91171526dc47764233b9b0f64
Josh Espinoza <joshespi@gmail.com>
2021-12-05 23:44:34 -0700

    added portfolio tracking

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
            <h3>Parentally Challenged Podcast</h3>
            <a href="https://parentallychallengedpodcast.com/">Parentally Challenged Podcast</a>
          </article>
+      <h2>Referral Links</h2>
+        <h3>Portfolio Tracking</h3>
+          <article>
+            <h4>cointracker.io</h4>
+            <p><a href="https://www.cointracker.io/i/B87LUdDgl2ZS">Great app for tracking your cost basis and getting ready for taxes</a></p>
+          </article>
      </section>
      <?php
        include("template/footer.php");
