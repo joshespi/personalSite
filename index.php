0a8d2029c637c13950d16a70eb3b10cae538b317
Josh Espinoza <joshespi@gmail.com>
2021-12-05 23:44:12 -0700

    added my projects

--- index.php
+++ index.php
@@ -2,14 +2,11 @@
   include("template/header.php");
  ?>
     <section id="siteContent">
-
-<!--
-     <article>
-       <img src="" alt="" />
-       <a href="resume.php">My Resume</a>
-
-     </article>
--->
+      <h2>My Projects</h2>
+         <article>
+           <h3>Parentally Challenged Podcast</h3>
+           <a href="https://parentallychallengedpodcast.com/">Parentally Challenged Podcast</a>
+         </article>
      </section>
      <?php
        include("template/footer.php");
