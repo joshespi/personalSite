4aab591615568cb12f0a98d3d97e2274cae504c5
Josh Espinoza <joshespi@gmail.com>
2021-12-05 23:45:03 -0700

    added stock investing refs

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
             <h4>cointracker.io</h4>
             <p><a href="https://www.cointracker.io/i/B87LUdDgl2ZS">Great app for tracking your cost basis and getting ready for taxes</a></p>
           </article>
+        <h3>Stock Investing</h3>
+          <article>
+            <h4>Webull</h4>
+            <p>Limited time offer: <a href="https://a.webull.com/8XVZUBSD2cWyWHx1l4">Open an account with Webull and get a free stock! Fund it with any amount and get one more!</a></p>
+          </article>
+          <article>
+            <h4>Robinhood</h4>
+            <p><a href="https://join.robinhood.com/joshe1026">Join Robinhood with my link and we'll both get a free stock</a></p>
+          </article>
      </section>
      <?php
        include("template/footer.php");
