1bcb73e419e4b199aac52677b5c80024b5f0faa9
Josh Espinoza <joshespi@gmail.com>
2021-12-05 23:48:26 -0700

    header and nav rebuilt

--- template/header.php
+++ template/header.php
@@ -1,5 +1,4 @@
 <!doctype html>
-
 <html lang="en">
 <head>
   <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -8,10 +7,8 @@
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
     gtag('config', 'UA-198253017-1');
   </script>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>I Am Josh Espinoza</title>
@@ -25,33 +22,24 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Genos&family=Indie+Flower&family=Press+Start+2P&family=Source+Code+Pro&display=swap" rel="stylesheet">
-<!--  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+  <!--  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
   <!-- End Fonts -->
-
-  <link rel="stylesheet" href="assets/css/styles.css?v=0.0.1">
+  <link rel="stylesheet" href="assets/css/styles.css?v=0.0.4">
 </head>
-
 <body>
+  <header id="mainHeader">
+        <a href="index.php">
+          <h1>I Am Josh Espinoza</h1>
+          <img src="assets/img/Espi-Eth-lasereye.jpg" alt="Josh" />
+        </a>
+  </header>
+  <nav id="navbar">
+     <a href="https://twitter.com/The_Espi">My Twitter</a>
+     <a href="https://discord.gg/g9VrnM59Ut">Join My Discord</a>
+     <a href="https://www.youtube.com/user/Joshespi">My Youtube</a>
+     <a href="https://www.linkedin.com/in/josh-espinoza-200ab7210?trk=public-profile-badge-profile-badge-view-profile-cta">My linkedin</a>
+     <a href="https://github.com/joshespi">My Github</a>
+     <a href="https://gist.github.com/joshespi">My Gists</a>
+     <a href="https://iamjoshespinoza.com/wordpress/contact-me/">Contact Me</a>
+   </nav>
   <main id="siteWrapper">
-   <header id="mainHeader">
-         <a href="#" class="nav-logo">
-           <h1>I Am Josh Espinoza</h1>
-           <img src="assets/img/Espi-Eth-lasereye.jpg" alt="Josh" />
-         </a>
-   </header>
-   <nav class="navbar">
-
-                <a href="https://twitter.com/The_Espi">My Twitter</a>
-
-                <a href="https://discord.gg/g9VrnM59Ut">Join My Discord</a>
-
-                <a href="https://www.youtube.com/user/Joshespi">My Youtube</a>
-
-                <a href="https://www.linkedin.com/in/josh-espinoza-200ab7210?trk=public-profile-badge-profile-badge-view-profile-cta">My linkedin</a>
-
-                <a href="https://parentallychallengedpodcast.com/">Parentally Challenged Podcast</a>
-
-                <a href="https://github.com/joshespi">My Github</a>
-
-                <a href="https://gist.github.com/joshespi">My Gists</a>
-    </nav>
