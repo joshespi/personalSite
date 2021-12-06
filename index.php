97f62f35b72158677fcbda5ec78c7119f09d3f92
Josh Espinoza <joshespi@gmail.com>
2021-12-05 23:45:16 -0700

    crypto investing refs

--- index.php
+++ index.php
@@ -22,6 +22,28 @@
             <h4>Robinhood</h4>
             <p><a href="https://join.robinhood.com/joshe1026">Join Robinhood with my link and we'll both get a free stock</a></p>
           </article>
+        <h3>Crypto Investing</h3>
+          <article>
+            <h4>Blockfi</h4>
+            <p>BlockFi makes it so easy to buy/sell crypto and get loans! <a href="https://blockfi.com/?ref=c6489b48">Join BlockFi now to get a bonus when you fund your account!</a> </p>
+          </article>
+          <article>
+            <h4>Coinbase</h4>
+              <p><a href="https://www.coinbase.com/join/espino_f8t?src=android-link">Sign up for Coinbase using my link and we can each get $10 in Bitcoin</a></p>
+          </article>
+          <article>
+            <h4>Crypto.com</h4>
+            <p>Use my referral link to <a href="https://crypto.com/app/gkadv277cy">sign up for Crypto.com and we both get $25 USD</a> :)</p>
+          </article>
+          <article>
+            <h4>Gemini</h4>
+            <p>Have you seen the Gemini app? <a href="https://gemini.com/share/6nqqelnfe">Try it using my code and we'll each get $10 USD of Bitcoin.</a></p>
+          </article>
+          <article>
+            <h4>Voyager</h4>
+            <p>I'm inviting you to start investing in crypto with Voyager. Download the app and trade $100 to get $25 of free Bitcoin.
+            <p><a href="https://voyager.onelink.me/WNly/referral?af_sub5=JOS686D6A">Use code JOS686D6A or this link to claim your BTC</a></p>
+          </article>
      </section>
      <?php
        include("template/footer.php");
