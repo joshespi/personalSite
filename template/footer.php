9395ee1d8905a343c89072575c072d1d8d879e23
Josh Espinoza <joshespi@gmail.com>
2021-12-05 23:47:29 -0700

    added crypto wallets

--- template/footer.php
+++ template/footer.php
@@ -4,6 +4,9 @@
 </aside>
 <footer id="mainFooter">
 
+    <h2>Crypto Wallets</h2>
+    <p>BTC - 3MvjFQNAUEYEwXwAM3W4yhCn3WgbsfxzRQ</p>
+    <p>Eth - 0x370E4D177B71cfe1a7F3C1A7cD0aDcEa944C96a7</p>
 </footer>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="assets/js/scripts.js?v=0.0.1"></script>
