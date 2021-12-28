7ff579471c13eef932d08c194f6688c6f8261b8f
Josh Espinoza <joshespi@gmail.com>
2021-12-28 14:25:06 -0700

    moved referral info to its own page. updated the links on the index. added additional information

--- index.php
+++ index.php
@@ -2,11 +2,33 @@
   include("template/header.php");
  ?>
     <section id="siteContent">
+      <h2>Investing</h2>
+        <article>
+         <h3>My Crypto Portfolio</h3>
+         <p>Check out my crypto portfolio using crytpotracker.io</p>
+         <a href="https://www.cointracker.io/portfolio/1b1e8730-9ba8-4bc1-9785-9e6e2a9ec04d">My Crypto Portfolio</a>
+         <p><a href="https://www.cointracker.io/i/B87LUdDgl2ZS">Get a $10 credit for future purchases for each friend that buys a tax plan on CoinTracker</a></p>
+        </article>
+        <article>
+         <h3>My Stock Portfolio</h3>
+         <p>Check out my stock portfolio using Track My Dividends</p>
+         <a href="https://trackyourdividends.com/dashboard/viewportfolio/6GbXs8KSjUdUfZa3Pm2y5X5ZyV6p0Ovk">My Stock Portfolio</a>
+        </article>
+        <article>
+         <h3>Getting Started</h3>
+         <a href="referrals.php">Open up your first investing account using one of my referral links</a>
+        </article>
       <h2>My Projects</h2>
         <h3>Personal Projects</h3>
           <article>
            <h4>Parentally Challenged Podcast</h4>
+           <p>My wife and I have been starting to post about our experiances with raising a child with Autism. We haven't been keeping a schedule, but we plant to start posting again</p>
            <a href="https://parentallychallengedpodcast.com/">Parentally Challenged Podcast</a>
+          </article>
+          <article>
+           <h4>My Youtube</h4>
+           <p>Sometimes I post gameplay video, dashcam footage, podcast entries</p>
+           <a href="https://www.youtube.com/user/Joshespi">My Youtube Channel</a>
           </article>
           <!--
         <h3>Projects I've Contributed to</h3>
@@ -15,63 +37,7 @@
            <a href="https://parentallychallengedpodcast.com/">Parentally Challenged Podcast</a>
           </article>
         -->
-      <h2>Referral Links</h2>
-        <h3>Portfolio Tracking</h3>
-          <article>
-            <h4>cointracker.io</h4>
-            <p><a href="https://www.cointracker.io/i/B87LUdDgl2ZS">Great app for tracking your cost basis and getting ready for taxes</a></p>
-          </article>
-        <h3>Stock Investing</h3>
-          <article>
-            <h4>Webull</h4>
-            <p>this is what I personally use for my RothIRA</p>
-            <p>Limited time offer: <a href="https://a.webull.com/8XVZUBSD2cWyWHx1l4">Open an account with Webull and get a free stock! Fund it with any amount and get one more!</a></p>
-          </article>
-          <article>
-            <h4>Robinhood</h4>
-            <p><a href="https://join.robinhood.com/joshe1026">Join Robinhood with my link and we'll both get a free stock</a></p>
-          </article>
-        <h3>Crypto Investing</h3>
-          <article>
-            <h4>Blockfi</h4>
-            <p>BlockFi makes it so easy to buy/sell crypto and get loans! <a href="https://blockfi.com/?ref=c6489b48">Join BlockFi now to get a bonus when you fund your account!</a> </p>
-          </article>
-          <article>
-            <h4>Coinbase</h4>
-              <p><a href="https://www.coinbase.com/join/espino_f8t?src=android-link">Sign up for Coinbase using my link and we can each get $10 in Bitcoin</a></p>
-          </article>
-          <article>
-            <h4>Crypto.com</h4>
-            <p>Use my referral link to <a href="https://crypto.com/app/gkadv277cy">sign up for Crypto.com and we both get $25 USD</a> :)</p>
-          </article>
-          <article>
-            <h4>Gemini</h4>
-            <p>Have you seen the Gemini app? <a href="https://gemini.com/share/6nqqelnfe">Try it using my code and we'll each get $10 USD of Bitcoin.</a></p>
-          </article>
-          <article>
-            <h4>Voyager</h4>
-            <p>I'm inviting you to start investing in crypto with Voyager. Download the app and trade $100 to get $25 of free Bitcoin.
-            <p><a href="https://voyager.onelink.me/WNly/referral?af_sub5=JOS686D6A">Use code JOS686D6A or this link to claim your BTC</a></p>
-          </article>
-        <h3>Crypto Projects</h3>
-          <article>
-            <h4>Brave Browser</h4>
-              <p>
-              <a href="https://brave.com/">Learn More About Brave</a>.
-              </p>
-          </article>
-          <article>
-            <h4>Pi Network</h4>
-              <p>
-                Pi is a new digital currency developed by Stanford PhDs, with over 25 million members worldwide. <a href="https://minepi.com/Espi2988">claim your Pi, follow use my username (Espi2988) as your invitation code</a>.
-              </p>
-          </article>
-          <article>
-            <h4>BEE Network</h3>
-              <p>
-                <a href="https://bee.com/en/download">Download BEE Network. Use the invitation code: coinspi.</a>
-              </p>
-          </article>
+
      </section>
      <?php
        include("template/footer.php");
---
+++ referrals.php
@@ -0,0 +1,65 @@
+<?php
+  include("template/header.php");
+ ?>
+    <section id="siteContent">
+      <h2>Referral Links</h2>
+        <h3>Portfolio Tracking</h3>
+          <article>
+            <h4>cointracker.io</h4>
+            <p><a href="https://www.cointracker.io/i/B87LUdDgl2ZS">Great app for tracking your cost basis and getting ready for taxes</a></p>
+          </article>
+        <h3>Stock Investing</h3>
+          <article>
+            <h4>Webull</h4>
+            <p>this is what I personally use for my RothIRA</p>
+            <p>Limited time offer: <a href="https://a.webull.com/8XVZUBSD2cWyWHx1l4">Open an account with Webull and get a free stock! Fund it with any amount and get one more!</a></p>
+          </article>
+          <article>
+            <h4>Robinhood</h4>
+            <p><a href="https://join.robinhood.com/joshe1026">Join Robinhood with my link and we'll both get a free stock</a></p>
+          </article>
+        <h3>Crypto Investing</h3>
+          <article>
+            <h4>Blockfi</h4>
+            <p>BlockFi makes it so easy to buy/sell crypto and get loans! <a href="https://blockfi.com/?ref=c6489b48">Join BlockFi now to get a bonus when you fund your account!</a> </p>
+          </article>
+          <article>
+            <h4>Coinbase</h4>
+              <p><a href="https://www.coinbase.com/join/espino_f8t?src=android-link">Sign up for Coinbase using my link and we can each get $10 in Bitcoin</a></p>
+          </article>
+          <article>
+            <h4>Crypto.com</h4>
+            <p>Use my referral link to <a href="https://crypto.com/app/gkadv277cy">sign up for Crypto.com and we both get $25 USD</a> :)</p>
+          </article>
+          <article>
+            <h4>Gemini</h4>
+            <p>Have you seen the Gemini app? <a href="https://gemini.com/share/6nqqelnfe">Try it using my code and we'll each get $10 USD of Bitcoin.</a></p>
+          </article>
+          <article>
+            <h4>Voyager</h4>
+            <p>I'm inviting you to start investing in crypto with Voyager. Download the app and trade $100 to get $25 of free Bitcoin.
+            <p><a href="https://voyager.onelink.me/WNly/referral?af_sub5=JOS686D6A">Use code JOS686D6A or this link to claim your BTC</a></p>
+          </article>
+        <h3>Crypto Projects</h3>
+          <article>
+            <h4>Brave Browser</h4>
+              <p>
+              <a href="https://brave.com/">Learn More About Brave</a>.
+              </p>
+          </article>
+          <article>
+            <h4>Pi Network</h4>
+              <p>
+                Pi is a new digital currency developed by Stanford PhDs, with over 25 million members worldwide. <a href="https://minepi.com/Espi2988">claim your Pi, follow use my username (Espi2988) as your invitation code</a>.
+              </p>
+          </article>
+          <article>
+            <h4>BEE Network</h3>
+              <p>
+                <a href="https://bee.com/en/download">Download BEE Network. Use the invitation code: coinspi.</a>
+              </p>
+          </article>
+     </section>
+     <?php
+       include("template/footer.php");
+      ?>
