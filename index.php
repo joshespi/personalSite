436ea25cc330ac4c6ae2fc29595f274fccbc39dd
Josh Espinoza <joshespi@gmail.com>
2022-03-18 22:08:47 -0600

    removed article tiles
    not being used

--- index.php
+++ index.php
@@ -2,40 +2,25 @@
   include("template/header.php");
  ?>
     <section id="siteContent">
-      <h2>Investing</h2>
-        <article>
-         <h3>My Crypto Portfolio</h3>
-         <p>Check out my crypto portfolio using crytpotracker.io</p>
-         <a href="https://www.cointracker.io/portfolio/1b1e8730-9ba8-4bc1-9785-9e6e2a9ec04d">My Crypto Portfolio</a>
-         <p><a href="https://www.cointracker.io/i/B87LUdDgl2ZS">Get a $10 credit for future purchases for each friend that buys a tax plan on CoinTracker</a></p>
-        </article>
-        <article>
-         <h3>My Stock Portfolio</h3>
-         <p>Check out my stock portfolio using Track My Dividends</p>
-         <a href="https://trackyourdividends.com/dashboard/viewportfolio/6GbXs8KSjUdUfZa3Pm2y5X5ZyV6p0Ovk">My Stock Portfolio</a>
-        </article>
-        <article>
-         <h3>Getting Started</h3>
-         <a href="referrals.php">Open up your first investing account using one of my referral links</a>
-        </article>
-      <h2>My Projects</h2>
-        <h3>Personal Projects</h3>
+          <!--
           <article>
-           <h4>Parentally Challenged Podcast</h4>
-           <p>My wife and I have been starting to post about our experiances with raising a child with Autism. We haven't been keeping a schedule, but we plant to start posting again</p>
-           <a href="https://parentallychallengedpodcast.com/">Parentally Challenged Podcast</a>
+          <h4>Parentally Challenged Podcast</h4>
+          <a href="https://parentallychallengedpodcast.com/">Parentally Challenged Podcast</a>
           </article>
+
           <article>
-           <h4>My Youtube</h4>
+           <h2>My Youtube</h2>
            <p>Sometimes I post gameplay video, dashcam footage, podcast entries</p>
            <a href="https://www.youtube.com/user/Joshespi">My Youtube Channel</a>
           </article>
-          <!--
-        <h3>Projects I've Contributed to</h3>
+
           <article>
-           <h4>Parentally Challenged Podcast</h4>
-           <a href="https://parentallychallengedpodcast.com/">Parentally Challenged Podcast</a>
+           <h2>Getting Started with Investing</h2>
+           <a href="referrals.php">Open up your first investing account using one of my referral links</a>
           </article>
+
+
+      
         -->
 
      </section>
