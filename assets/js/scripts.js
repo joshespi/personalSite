80ec39c701b90d23b340d8a17e0f04b96a2c5ad6
Josh Espinoza <joshespi@gmail.com>
2022-08-14 22:19:50 -0600

    simplified website. switched to normalize from reset. removed redundant content

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,20 @@ $("#navbar > a").click(function () {
   $("#navbar ul").toggleClass("active");
 });
 
+$("#navbar > a").on("click", function () {
+  $(this).html(
+    $("#navbar ul").hasClass("active")
+      ? '<img src="assets/img/svg/exit.svg" alt="" />'
+      : '<img src="assets/img/svg/menu.svg" alt="" />'
+  );
+});
+
 //window resize event listener
 $(window).resize(function () {
   $("#navbar ul").removeClass("active");
+  $("#navbar > a").html('<img src="assets/img/svg/menu.svg" alt="" />');
+});
+
+$(document).ready(function () {
+  $("#navbar > a").html('<img src="assets/img/svg/menu.svg" alt="" />');
 });
